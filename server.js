ec8e17f0eddf0689f8ced7ee38241dadc9d0e785
chess2022 <lakeshore.hnh@gmail.com>
2022-06-03 18:32:50 -0700

    Updates routes and removes auth

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require("dotenv").config()
 const { PORT = 3002, DATABASE_URL } = process.env;
 const express = require("express")
 const app = express()
-const bcrypt = require("bcrypt");
 const mongoose = require("mongoose")
 const User = require("./models/customer")
 const Payment = require("./models/customer")
@@ -53,19 +52,20 @@ app.get("/signup", async (req, res) => {
 
 // signup send user data to server and stores in DB - POST
 app.post("/signup", async (req, res) => {
-  // // capture password and hash it
-  // req.body.password = await bcrypt.hash(
-  //   req.body.password,
-  //   await bcrypt.genSalt(10)
-  // );
-
   // create the user in the DB
   try {
     res.json(await User.create(req.body));
   } catch (error) {
     res.status(400).json(error);
   }
+    res.redirect("/success")
 });
+
+
+// success page after signup to link to login
+app.get("/success", (req, res) => {
+  console.log("Success!")
+})
 
 // log in page route - GET
 app.get("/login", async (req, res) => {
@@ -74,61 +74,46 @@ app.get("/login", async (req, res) => {
       } catch (error) {
         res.status(400).json(error);
       };
+        res.redirect("/restaurants");
+
 });
 
-// login send user data to server - POST
-// app.post("/login", (req, res) => {
-  // look the user up by username in DB
-  // const { username, password } = req.body;
-  // User.findOne( username, async (err, user) => {
-    // if user doesn't exist send error message
-    // if (err || !user) return res.send("User does not exist"); //can also render an ejs page that says cannot find that account & redirects to proper page
-    // compare password
-    // const passwordMatches = await bcrypt.compare(password, user.password); //first comes from frontend and second from DB
-    // or if it does not match redirect to login - error message
-    // if (!passwordMatches) return res.send("Incorrect password"); //or res.render a page and redirects to login
-    // save login status in session/create session
-    // req.session.loggedIn = true;
-    // req.session.username = username;
-    // redirect to restaurants to see options
-//     res.redirect("/restaurants");
-//   });
-// });
+// ORDERS INDEX
+app.get("/order", async (req, res) => {
+  try {
+    res.json(await Order.find({}))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
 
-// end session (logout) uses express session route to delete that session ID and redirect back home
-app.get("/logout", (req, res) => {
-  req.session.destroy((err) => {
-    res.redirect("/login");
-  });
-});
-
-// USER DASHBOARD
-app.get("/user", async (req, res) => {
-      try {
-        res.json(await User.find({}))
-      } catch (error) {
-        res.status(400).json(error);
-      }
+// ORDER SHOW
+app.get("/order/:id", async (req, res) => {
+  try {
+    res.json(await Order.find(req.params.id));
+  } catch (error) {
+    res.status(400).json(error);
+  }
 })
 
 
-// USER DELETE ACCOUNT ROUTE
-app.delete("/user/:id", async (req, res) => {
+// ORDER DELETE ROUTE
+app.delete("/order/:id", async (req, res) => {
     try {
         // send all users
-        res.json(await User.findByIdAndDelete(req.params.id))
+        res.json(await Order.findByIdAndDelete(req.params.id))
     } catch (error) {
         //send error
         res.status(400).json(error)
     }
 })
 
-// USER UPDATE ROUTE
-app.put("/user/:id", async (req, res) => {
+// ORDER UPDATE ROUTE
+app.put("/order/:id", async (req, res) => {
     try {
         // send all users
         res.json(
-        await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        await Order.findByIdAndUpdate(req.params.id, req.body, { new: true })
         )
     } catch (error) {
         //send error
